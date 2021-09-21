de76cea393f908a080d221e3e6621b727b74f0e8
João Paulo <jpaulope@indracompany.com>
2021-09-21 03:19:50 -0300

    docs: redirect user to api documentation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
 import { Router, Request, Response } from 'express';
 const routes = Router();
 
+routes.get('/', (req: Request, resp: Response) => {
+  resp.redirect('/docs');
+});
+
 routes.get('/contatos', (req: Request, resp: Response) => {
   return resp.json('hi');
 });
