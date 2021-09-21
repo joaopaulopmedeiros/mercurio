71aad992a1ff9e0d14dc583fa2f3f610d8d62ed3
João Paulo <jpaulope@indracompany.com>
2021-09-21 03:12:14 -0300

    docs: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello world');
+import express from 'express';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
+import routes from './routes/index';
+
+const app = express();
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router, Request, Response } from 'express';
+const routes = Router();
+
+routes.get('contatos', (req: Request, resp: Response) => {
+  return resp.json('hi');
+});
+
+export default routes;
