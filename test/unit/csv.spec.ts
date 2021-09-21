e34757bc737c32cd55436b615877732e35469893
João Paulo <jpaulope@indracompany.com>
2021-09-21 00:21:38 -0300

    tests: remove unused code

--- test/unit/csv.spec.ts
+++ test/unit/csv.spec.ts
@@ -3,20 +3,18 @@ import ImportCsvFileService from '../../src/services/ImportCsvFileService';
 
 describe('csv import', () => {
   test('it should import a csv file and update database with contacts', async() => {
-    // arrange
     const testData = [
       'maria,maria@gmail.com'
     ];
+
     const [expectedName, expectedEmail] = testData[0].split(',');
 
     const fileStream = Readable.from(testData);
 
     const service = new ImportCsvFileService();
 
-    // act
     const [result] = await service.run(fileStream);
 
-    // assert
     expect(result).toEqual({ name: expectedName, email: expectedEmail });
   });
 });
