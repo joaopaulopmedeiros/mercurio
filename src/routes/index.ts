988966c637d93e150d293f19e76bca15e87d3f5c
João Paulo <jpaulope@indracompany.com>
2021-09-21 03:21:33 -0300

    refactor: http context variable names

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router, Request, Response } from 'express';
 const routes = Router();
 
-routes.get('/', (req: Request, resp: Response) => {
-  resp.redirect('/docs');
+routes.get('/', (request: Request, response: Response) => {
+  response.redirect('/docs');
 });
 
-routes.get('/contatos', (req: Request, resp: Response) => {
-  return resp.json('hi');
+routes.get('/contatos', (request: Request, response: Response) => {
+  return response.json('hi');
 });
 
 export default routes;
