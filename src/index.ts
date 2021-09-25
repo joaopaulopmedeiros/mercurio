b48657267a2dd6102108b4d58ea1f18cca5bfb30
João Paulo <jpaulope@indracompany.com>
2021-09-25 12:01:46 -0300

    chore: grant mongodb connection on http server

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../swagger.json';
 import routes from './api/routes/index';
+import ConnectionManager from './infra/database/mongodb/ConnectionManager';
 
+//mongodb
+const mongodbConnectionManager = new ConnectionManager();
+mongodbConnectionManager.initializeMongoDBConnection();
+
+//http
 const app = express();
-
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 app.listen(3333);
